445113208431078ae07585621db954b311eb3476
Harshitha S Kanala <harshithasubramanyam2001@gmail.com>
2025-02-05 09:46:05 +0530

    config db.js updated

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected',() => {
+    mongoose.connection.on('connected', () => {
         console.log("DB Connected");
-    })
+    });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
-
-}
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
 
 export default connectDB;
