20828e2ece05cd7e488e4f3972f03f43f15826fa
Harshitha S Kanala <harshithasubramanyam2001@gmail.com>
2025-02-05 10:39:52 +0530

    CORS FIXES

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,8 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-const allowedOrigins = ['https://weebtales-iota.vercel.app'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type, Authorization', 
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 
 // api endpoints
